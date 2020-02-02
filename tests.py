edd2983a46ad39f64c6a824f5c6cf0c149458c26
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-02 09:33:47 +0100

    Added test

--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
 import unittest
 from settings import Settings
+from alien_invasion import AlienInvasion
 
 
 class TestSettings(unittest.TestCase):
-    """Tests the whole game AlienInvasion"""
+    """Tests the settings for AlienInvasion"""
 
     def setUp(self):
         """attributes for settings"""
@@ -16,6 +17,19 @@ class TestSettings(unittest.TestCase):
     def test_screen_bg_color(self):
         """Tests background color"""
         self.assertEqual(self.settings.bg_color,(230,230,230))
+
+"""
+Cannot test run_game() because terminal gets blocked
+
+class TestAlienInvasion(unittest.TestCase):
+    
+
+    def setUp(self):
+        self.ai = AlienInvasion()
+
+    def test_run_game(self):
+        
+        self.assertEqual(self.screen.fill(self.settings.bg_color),self.screen.fill((230,230,230)))
 
 if __name__ == "__main__":
     unittest.main()
